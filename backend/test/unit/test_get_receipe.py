e7bc18f715f790b54df5a83d1930cc16a6d6fbe2
Niklas Johansson <rapsabamse1@gmail.com>
2023-05-22 10:56:53 +0100

    2

--- backend/test/unit/test_get_receipe.py
+++ backend/test/unit/test_get_receipe.py
@@ -1,10 +1,12 @@
 import pytest
 import unittest.mock as mock
 
-from src.controllers.receipecontroller import ReceipeController #doesnt work
-# add your test case implementation here
+from src.controllers.receipecontroller import ReceipeController #doesnt work,
+# tried using ...src.controllers.receipecontroller but that doesnt work either.
 
 ## I didnt get the pathing to work so im not 100% sure that the code below is working correctly since I couldnt test it.
+
+#Readiness over 0.1, correct diet
 @pytest.mark.unit
 def test_readiness_Over():
     mockedcalculate_readiness = mock.MagicMock()
@@ -19,6 +21,7 @@ def test_readiness_Over():
 
     assert validationResult == 1
 
+##readiness 0 (under 0.1), correct diet
 @pytest.mark.unit
 def test_readiness_Under():
     mockedcalculate_readiness = mock.MagicMock()
@@ -33,6 +36,7 @@ def test_readiness_Under():
 
     assert validationResult == 1
 
+#Tries giving a recipe not for vegetarians to a vegetarian
 @pytest.mark.unit
 def test_Incorrect_diet():
     mockedcalculate_readiness = mock.MagicMock()
